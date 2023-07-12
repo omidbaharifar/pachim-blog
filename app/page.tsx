edfb384b718a6f72834a7054213b3ebf8ea37de0
omidbaharifar <weblax.ir@gmail.com>
2023-07-12 07:23:16 +0330

    feat: create home content component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import HomeContent from "@/domain/home";
+
 const HomePage = () => {
-  return <main className="bg-coustomGray-200 min-h-screen text-white"></main>;
+  return (
+    <main className="bg-coustomGray-200 min-h-screen text-white flex justify-center items-center">
+      <HomeContent />
+    </main>
+  );
 };
 
 export default HomePage;
---
+++ domain/home/index.tsx
@@ -0,0 +1,9 @@
+const HomeContent = () => {
+  return (
+    <ul className="flex justify-center items-center gap-40">
+      <li className="w-full max-w-xs">test</li>
+    </ul>
+  );
+};
+
+export default HomeContent;
