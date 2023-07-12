15b8cfd00969bd9ce85e54276206b964414eec5d
omidbaharifar <weblax.ir@gmail.com>
2023-07-12 09:41:58 +0330

    feat: card styles added

--- components/cards/blogCard/index.tsx
+++ components/cards/blogCard/index.tsx
@@ -9,9 +9,16 @@ interface props {
 
 const BlogCard: FC<props> = ({ blog, large }) => {
   return (
-    <li>
-      <Link href={"/" + blog.slug} className="w-full block">
-        <div className="group overflow-hidden rounded-xl">
+    <li className="h-full">
+      <Link
+        href={"/" + blog.slug}
+        className={`w-full flex flex-col h-full ${
+          large
+            ? ""
+            : "border-2 border-black/10 border-solid rounded-xl overflow-hidden"
+        }`}
+      >
+        <div className={`group overflow-hidden ${large ? "rounded-xl" : ""}`}>
           <Image
             src={blog.image}
             alt={blog.title}
@@ -20,15 +27,27 @@ const BlogCard: FC<props> = ({ blog, large }) => {
             className="aspect-video object-cover object-center w-full group-hover:scale-105 transition-all"
           />
         </div>
-        <h2 className="text-4xl font-bold my-10 leading-relaxed">
+        <h2
+          className={`font-bold leading-relaxed ${
+            large ? "text-4xl my-10" : "text-xl m-5"
+          }`}
+        >
           {blog.title}
         </h2>
-        <p className="text-xl whitespace-nowrap text-ellipsis overflow-hidden">
+        <p
+          className={`whitespace-nowrap text-ellipsis overflow-hidden mb-5 ${
+            large ? "text-xl" : "text-base mx-5"
+          }`}
+        >
           {blog.short}
         </p>
-        <div className="flex justify-start items-center text-base mt-5">
+        <div
+          className={`flex justify-start items-center mt-auto ${
+            large ? "mb-10 text-base" : "mb-5 mx-5 text-sm"
+          }`}
+        >
           <div className="flex justify-center items-center gap-3 pl-3">
-            <div className="rounded-full overflow-hidden w-10 h-10">
+            <div className="rounded-full overflow-hidden w-10 h-10 flex-none">
               <Image
                 src={blog.author.image}
                 alt={blog.author.name}
@@ -39,7 +58,9 @@ const BlogCard: FC<props> = ({ blog, large }) => {
             </div>
             <h3>{blog.author.name}</h3>
           </div>
-          <p className="opacity-50 pr-3 border-r-2 border-solid border-black/50">{blog.date}</p>
+          <p className="opacity-50 pr-3 border-r-2 border-solid border-black/50">
+            {blog.date}
+          </p>
         </div>
       </Link>
     </li>
