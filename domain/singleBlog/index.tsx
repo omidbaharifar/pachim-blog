cb72d9e73d2f2a471c0848d69542749bdeed4814
omidbaharifar <weblax.ir@gmail.com>
2023-07-12 21:14:30 +0330

    feat: limit moreblogs to 3

--- domain/singleBlog/index.tsx
+++ domain/singleBlog/index.tsx
@@ -9,7 +9,7 @@ interface props {
 }
 
 const SingleBlogContent: FC<props> = ({ blogData }) => {
-  const moreBlogsList = blogList.filter((item) => item.slug !== blogData.slug);
+  const moreBlogsList = blogList.filter((item) => item.slug !== blogData.slug).splice(0,3);
 
   return (
     <main className="max-w-screen-xl w-full mx-auto px-5 lg:px-10 mb-10 lg:mb-20 mt-5 lg:mt-10">
