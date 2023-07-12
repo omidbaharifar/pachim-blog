168f58e7e630cec1da117cce23c1fd1d81ac4ef5
omidbaharifar <weblax.ir@gmail.com>
2023-07-12 07:05:57 +0330

    feat: add domain as valid direction in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./domain/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
