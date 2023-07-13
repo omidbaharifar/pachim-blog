82490857c4ba658c9163577375c63174cc9728c8
Hesam Mousavi <hesammoousavi@gmail.com>
2023-07-13 10:27:56 +0330

    add .env properties to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env : {
+        NEXT_PUBLIC_CLIENT_URL : process.env.NEXT_PUBLIC_CLIENT_URL
+    }
+}
 
 module.exports = nextConfig
